ff00b8f3f023c80a1ea4c0b5e9cfed1522e90b04
thuanCEO <dohuuthuan.bhdn@gmail.com>
2024-03-21 16:47:51 +0700

    update sql

--- WebAppAPI/obj/Debug/net8.0/WebAppAPI.AssemblyInfo.cs
+++ WebAppAPI/obj/Debug/net8.0/WebAppAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b106b4974954a5a9ac8a82aaf965b04876ef7183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5c8af8df87f6411f113da12631107161a72ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
