4e5c8af8df87f6411f113da12631107161a72ea6
thuanCEO <dohuuthuan.bhdn@gmail.com>
2024-03-21 16:06:45 +0700

    update code

--- WebAppAPI/obj/Debug/net8.0/WebAppAPI.AssemblyInfo.cs
+++ WebAppAPI/obj/Debug/net8.0/WebAppAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42014673aa8ae74850df22b12b50c26ab93e7c19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b106b4974954a5a9ac8a82aaf965b04876ef7183")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
